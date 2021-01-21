18e591588146d8cf1f539480260444cf62bed963
pawel5z <pawelszme@gmail.com>
2021-01-21 01:50:13 +0100

    Added killcam shots.

--- Assets/Scripts/PlayerDrillScript.cs
+++ Assets/Scripts/PlayerDrillScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Cinemachine;
 
 public class PlayerDrillScript : MonoBehaviour
 {
@@ -8,10 +9,16 @@ public class PlayerDrillScript : MonoBehaviour
     public float maxAngVel = 100;
     public GameObject drillFX;
     public AudioClip destroyClip;
+    public CinemachineVirtualCamera[] killCams;
 
     private void Start()
     {
         rb.maxAngularVelocity = maxAngVel;
+
+        GameObject[] killCamsObjects = GameObject.FindGameObjectsWithTag("Kill Cam");
+        killCams = new CinemachineVirtualCamera[killCamsObjects.Length];
+        for (int i = 0; i < killCamsObjects.Length; i++)
+            killCams[i] = killCamsObjects[i].GetComponent<CinemachineVirtualCamera>();
     }
 
     private void FixedUpdate()
@@ -38,7 +45,11 @@ public class PlayerDrillScript : MonoBehaviour
     private IEnumerator KillFX()
     {
         Time.timeScale = .5f;
+        CinemachineVirtualCamera tempCam = killCams[Random.Range(0, killCams.Length)];
+        int priority = tempCam.Priority;
+        tempCam.Priority += 10;
         yield return new WaitForSecondsRealtime(2f);
+        tempCam.Priority = priority;
         Time.timeScale = 1f;
     }
 }
